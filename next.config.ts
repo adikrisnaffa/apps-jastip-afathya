bda900633e9ffb3f2219fc10347d2a03b287073a
Adikrisna Nugraha <adikrisnanugraha@gmail.com>
2025-10-29 03:21:32 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'firebasestudio.app',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
